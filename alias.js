bcd4c5e83ab3b40d0387ef5b4e098d2102e73b1d
Matt Styles <matt@personalurban.com>
2020-04-30 14:58:34 +0100

    add preact render to string helper

--- alias.js
+++ alias.js
@@ -1,7 +1,11 @@
 
 const moduleAlias = require('module-alias')
+const renderToString = require('preact-render-to-string')
 
 module.exports = () => {
+  if (!renderToString.renderToStaticMarkup) {
+    renderToString.renderToStaticMarkup = renderToString
+  }
   moduleAlias.addAlias('react', 'preact/compat')
   moduleAlias.addAlias('react-dom', 'preact/compat')
   moduleAlias.addAlias('react-ssr-prepass', 'preact-ssr-prepass')
